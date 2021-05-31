2b87b4fdf7466cd172bdf9bccaaaf3abefde06ed
lijinfei <lijinfei@devcloud.com>
2021-05-31 18:03:40 +0800

    alert

--- example/src/App.vue
+++ example/src/App.vue
@@ -7,34 +7,33 @@
         below
       </p>
       <el-button>el-button</el-button>
-      <el-alert
-        title="成功提示的文案"
-        center
-        type="success">
+      <el-alert title="成功提示的文案" center type="success">
       </el-alert>
-      <d-alert center title="成功提示的文案"
-        type="success"></d-alert>
+      <d-alert center title="成功提示的文案" type="success"></d-alert>
+      <d-alert center title="成功提示的文案" type="error"></d-alert>
+      <d-alert center title="成功提示的文案" type="info"></d-alert>
+      <d-alert center title="成功提示的文案" type="warning"></d-alert>
     </div>
   </div>
 </template>
 
 <script>
-import DAlert from './package/Alert'
-export default {
-  name: 'app',
-  components: {
-    DAlert
+  import DAlert from './package/Alert'
+  export default {
+    name: 'app',
+    components: {
+      DAlert
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- example/src/package/Alert.vue
+++ example/src/package/Alert.vue
@@ -1,9 +1,12 @@
 <template>
-  <div v-show="visible" class="d-alert" :class="[typeClass, center ? 'center' : '']">
-      <div class="d-alert-content">
-        <div class="d-alert-title">{{title}}</div>
-      </div>
-  </div>
+  <transition name="d-alert">
+    <div v-show="visible" class="d-alert" :class="[typeClass, center ? 'center' : '']">
+        <div class="d-alert-content">
+            <div class="d-alert-title">{{title}}</div>
+            <i class="d-alert--closebtn" :class="{ 'is-customed': closeText !== '', 'el-icon-close': closeText === '' }" v-show="closable" @click="close()">{{closeText}}</i>
+        </div>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -18,8 +21,16 @@ export default {
             type: String,
             default: 'success',
             validator(val) {
-                return ['success', 'error', 'warning'].indexOf(val) !== -1;
+                return ['success', 'error', 'warning', 'info'].indexOf(val) !== -1;
             }
+        },
+        closable: {
+            type: Boolean,
+            default: true
+        },
+        closeText: {
+            type: String,
+            default: ''
         },
         desc: {
             type: String,
@@ -31,6 +42,12 @@ export default {
     data() {
         return {
             visible: true
+        }
+    },
+    methods: {
+        close() {
+            this.visible = false;
+            this.$emit('close')
         }
     },
     computed: {
@@ -48,6 +65,10 @@ export default {
         padding: 8px 16px;
         display: flex;
         align-items: center;
+        position: relative;
+        opacity: 1;
+        transition: opacity .7s;
+        overflow: hidden;
     }
     .d-alert.center {
         justify-content: center;
@@ -62,5 +83,33 @@ export default {
     .d-alert-success {
         background: #f0f9eb;
         color: #67C23A;
+    }
+    .d-alert-error {
+        background-color: #fef0f0;
+        color: #F56C6C;
+    }
+    .d-alert-info {
+        background-color: #f4f4f5;
+        color: #909399;
+    }
+    .d-alert-warning {
+        background-color: #fdf6ec;
+        color: #E6A23C;
+    }
+    .d-alert--closebtn {
+        font-size: 12px;
+        opacity: 1;
+        position: absolute;
+        top: 12px;
+        right: 15px;
+        cursor: pointer;
+    }
+    /* prop-enter 进入之前 prop-leave-to 离开之后 */
+    .d-alert-enter, .d-alert-leave-to {
+        opacity: 0;
+    }
+    /* prop-enter-to 进入之后 prop-leave 开始离开 */
+    .d-alert-enter-to, .d-alert-leave {
+        opacity: 1;
     }
 </style>
